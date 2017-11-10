574fcce6681dfa063be6717cccccbdbe2dff7888
Can Yuva <canyuva7@gmail.com>
2017-11-11 01:51:37 +0300

    Update

---
+++ PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int health = 100;
+	public Text healthText;
+	
+	void Start(){
+		
+		healthText.text = health.ToString() + " can kaldı.";
+	}
+	// Update is called once per frame
+	void Update (){
+		if(Input.GetKeyDown(KeyCode.K) && health != 0){
+			health -= 5;
+			healthText.text = health.ToString() + " can kaldı.";
+		}
+		if(health == 0)
+			die();
+	}
+	void die(){}
+}
---
+++ PlayerMovementwithJump.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+		// Components for your Player  || Player için gerekli bileşenler
+		// 1- Rigidbody
+		// 2- Box Collider for isGrounded checking || isGrounded kontrolü için box collider
+		
+
+
+	public float movementSpeed = 30.0f;
+	private float Vector3;
+	public float jumpSpeed = 50.0f;
+	public Rigidbody rb;
+	public float thrust = 300.0f;
+	public Collider cd;
+	public Transform transform;
+	public bool isGrounded = true;
+	float x, z;
+	void Start (){
+	
+		rb = GetComponent<Rigidbody>();
+		transform = GetComponent<Transform>();
+	
+	}
+	void Update () {
+		
+		if(isGrounded){
+			x = Input.GetAxis ("Horizontal") * Time.deltaTime * movementSpeed;
+			z = Input.GetAxis ("Vertical") * Time.deltaTime * movementSpeed;
+		}
+			transform.Translate (x, 0, z);
+
+		 if(Input.GetButtonDown("Jump")){
+			 
+			  
+			  if(isGrounded){
+				rb.AddForce(transform.up * thrust);
+			  }
+		 }
+		 
+	}
+	
+	void OnTriggerEnter(Collider cd){
+		isGrounded = true;
+	}
+	void OnTriggerExit(Collider cd){
+		isGrounded = false;
+	}
+}
