440c711acc0456a925edb50d442f0ef9b998386a
Can Yuva <canyuva06@gmail.com>
2017-11-14 19:44:37 +0300

    fixed

--- MouseLook.cs
+++ MouseLook.cs
@@ -8,13 +8,15 @@ public class MouseLook : MonoBehaviour {
  public float sensitivityY = 15F;
  public float minimumY = -60F;
  public float maximumY = 60F;
+ public Camera camera;
  float rotationY = 0F;
  float rotationX = 0f;
  void Update (){
      rotationX += Input.GetAxis ("Mouse X") * sensitivityX;
      rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
      rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-     transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0);
+     transform.localEulerAngles = new Vector3 (0, rotationX, 0);
+	 camera.transform.localEulerAngles = new Vector3 (-rotationY,0,0);
 	}
 		
 }
